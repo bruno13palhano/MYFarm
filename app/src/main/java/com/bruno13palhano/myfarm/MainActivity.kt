c1e1582cacb3d59bc4524890a4b7937e95d1b8d2
bruno13palhano <bruno13palhano@gmail.com>
2024-04-19 19:06:28 -0300

    Marks MainActivity for hilt injection

--- app/src/main/java/com/bruno13palhano/myfarm/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/myfarm/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.bruno13palhano.myfarm.ui.theme.MYFarmTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
