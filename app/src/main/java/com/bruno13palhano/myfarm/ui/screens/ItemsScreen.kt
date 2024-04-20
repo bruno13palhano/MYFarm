edfa2eb74efff83bd1c0f3c00935a62c539072b5
bruno13palhano <bruno13palhano@gmail.com>
2024-04-20 20:08:33 -0300

    Add a ExtendedFloatingActionButton to ItemsScreen

--- app/src/main/java/com/bruno13palhano/myfarm/ui/screens/ItemsScreen.kt
+++ app/src/main/java/com/bruno13palhano/myfarm/ui/screens/ItemsScreen.kt
@@ -4,23 +4,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.bruno13palhano.myfarm.R
 
 @Composable
-fun  ItemsRoute() { ItemsScreen() }
+fun ItemsRoute() { ItemsScreen() }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ItemsScreen() {
     Scaffold(
-        topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.items_label)) }) }
+        topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.items_label)) }) },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(id = R.string.add_button_description)
+                )
+                Text(
+                    modifier = Modifier.padding(start = 8.dp),
+                    text = stringResource(id = R.string.new_item_label)
+                )
+            }
+        }
     ) {
         Column(
             modifier = Modifier
