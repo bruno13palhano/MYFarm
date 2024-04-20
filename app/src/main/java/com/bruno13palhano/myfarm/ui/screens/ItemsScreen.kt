30df6c43dfd1b0d1229f2db2ae55c34192f20509
bruno13palhano <bruno13palhano@gmail.com>
2024-04-20 19:39:23 -0300

    Add ItemsScreen

---
+++ app/src/main/java/com/bruno13palhano/myfarm/ui/screens/ItemsScreen.kt
@@ -0,0 +1,33 @@
+package com.bruno13palhano.myfarm.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.bruno13palhano.myfarm.R
+
+@Composable
+fun  ItemsRoute() { ItemsScreen() }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ItemsScreen() {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = stringResource(id = R.string.items_label)) }) }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState())
+        ) {
+
+        }
+    }
+}
