14733dff3296b8a1f286152550d84b4352fb8db0
bruno13palhano <bruno13palhano@gmail.com>
2024-04-25 20:23:04 -0300

    Add compose lifecycle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,5 @@ dependencies {
     implementation("androidx.hilt:hilt-work:1.1.0")
     implementation("androidx.navigation:navigation-compose:$navVersion")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
 }
