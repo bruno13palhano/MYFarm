6f928dd7016575129e0da7fea11ac5cc4f161393
bruno13palhano <bruno13palhano@gmail.com>
2024-05-01 21:09:48 -0300

    Add update method

--- core/src/main/java/com/bruno13palhano/core/data/repository/Data.kt
+++ core/src/main/java/com/bruno13palhano/core/data/repository/Data.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 interface Data<T> {
     suspend fun insert(data: T)
     suspend fun delete(id: Long)
+    suspend fun update(data: T)
     fun getById(id: Long): Flow<T>
     fun getAll(): Flow<List<T>>
 }
