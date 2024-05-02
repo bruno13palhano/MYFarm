caacf2294e60d624569c6e07275f1af601be5427
bruno13palhano <bruno13palhano@gmail.com>
2024-05-01 21:10:25 -0300

    Add new properties to Item

--- core/src/main/java/com/bruno13palhano/core/model/Item.kt
+++ core/src/main/java/com/bruno13palhano/core/model/Item.kt
@@ -3,7 +3,10 @@ package com.bruno13palhano.core.model
 data class Item(
     val id: Long,
     val index: Int,
-    val successorsIndices: List<Int>,
+    val x: Float,
+    val y: Float,
+    val radius: Float,
+    var successorsIndices: List<Int>,
     val name: String,
     val description: String
 )
