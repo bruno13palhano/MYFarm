c15e3f5dad748203025599ff5d27c1388351f11a
bruno13palhano <bruno13palhano@gmail.com>
2024-05-04 20:55:37 -0300

    Add an id property and rename linesIndex to edges

--- app/src/main/java/com/bruno13palhano/myfarm/ui/common/Vertex.kt
+++ app/src/main/java/com/bruno13palhano/myfarm/ui/common/Vertex.kt
@@ -3,7 +3,8 @@ package com.bruno13palhano.myfarm.ui.common
 import androidx.compose.ui.geometry.Offset
 
 data class Vertex(
+    val id: Long = 0L,
     val center: Offset,
     val radius: Float = 40F,
-    val linesIndex: List<Int> = listOf()
+    var edges: List<Int> = listOf()
 )
