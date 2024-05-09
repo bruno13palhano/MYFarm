b60574cedf96c9738e3d739d88d63331e9087788
bruno13palhano <bruno13palhano@gmail.com>
2024-05-09 20:31:31 -0300

    Add a property for label and increase the radius size to 50

--- app/src/main/java/com/bruno13palhano/myfarm/ui/common/Vertex.kt
+++ app/src/main/java/com/bruno13palhano/myfarm/ui/common/Vertex.kt
@@ -4,7 +4,8 @@ import androidx.compose.ui.geometry.Offset
 
 data class Vertex(
     val id: Long = 0L,
+    val label: String = "",
     val center: Offset,
-    val radius: Float = 40F,
+    val radius: Float = 50F,
     var edges: List<Int> = listOf()
 )
